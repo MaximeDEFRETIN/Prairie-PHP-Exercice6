cf853d83e6f5be9a7b63cecbc46447c00b6c24ea
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-03 08:53:12 +0200

    Exercice 6

--- Exercice6.php
+++ Exercice6.php
@@ -1,5 +1,16 @@
 <?php
-function 
-langage = array("HTML","CSS","Javascript","PHP");
-
+$langage = array("HTML","CSS","Javascript","PHP");
+$Tab = TabOAlph($langage);
+foreach($Tab as $element) {
+  echo $element;
+}
+function TabOAlph($langage) {
+  sort($langage);
+  return $langage;
+}
+function string($langage) {
+  implode('', $langage);
+  //echo $langage;
+  return ($langage);
+}
 ?>
