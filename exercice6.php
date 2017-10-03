47cae0f39907255fa8048f41f171e51250295f70
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-03 15:42:11 +0200

    Exercice 6 corigé

---
+++ exercice6.php
@@ -0,0 +1,56 @@
+<?php
+//déclaration d'un tableau
+$langage = array("HTML","CSS","Javascript","PHP");
+//appel de la fonction TabOAlph
+$Tab = TabOAlph($langage);
+//parcours chaque le tableau pour afficher son contenu
+foreach($Tab as $element) {
+  echo $element;
+}
+//déclaration de la fonction TabOAlph() permettant de tirer par ordre alphabétique
+function TabOAlph($langage) {
+  //tri du tableau
+  sort($langage);
+  return $langage;
+}
+//déclaration de la fonction chain() qui permet de renvoyer le contenu d'un tableau concaténé
+function chain($langage) {
+  $chf = implode(' ', $langage); /*inverse() de implode c'est explode()*/
+  return($chf);
+}
+echo '<br />';
+$chf = chain($langage);
+echo $chf;
+?>
+
+/*<?php
+//déclaration du tableau $language
+$language = array("HTML","CSS","Javascript","PHP");
+//déclaration de la fonction alphabetic() qui permet de trier $language par ordre alphabétique
+function alphabetic($code){
+  //vérification que le paramètre est bien un tableau
+  if(is_array($code)==true){
+    //tri du tableau
+    sort($code);
+  }
+  return $code;
+}
+echo '<br/>Le tableau langage trié :<br/>';
+//on appelle la fonction alphabetic() et on stocke le résultat dans le tableau $sortedArray
+$sortedArray = alphabetic($language);
+//on parcours le tableau avec la boucle foreach pour afficher son contenu
+foreach($sortedArray as $value){
+  echo $value . '<br/>';
+}
+//déclaration de la fonction concat() qui permet de renvoyer le contenu d'un tableau concaténé
+function concat($code){
+  //vérification que le paramètre est bien un tableau
+  if(is_array($code)==true){
+    //Rassemble les éléments du tableau dans la chaîne $string
+    $string = implode(' ', $code);
+  }
+  return $string;
+}
+echo '<br/>Le tableau langage concaténé :<br/>';
+echo concat($language);
+?>*/
